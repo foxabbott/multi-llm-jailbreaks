00dbe40289ce75ffc0c6c354af2226551a6b719e
Kaspar Senft <senft.kaspar@gmail.com>
2024-03-17 21:27:43 +0000

    implemented custom openai backend with token counter

--- backend.py
+++ backend.py
@@ -1,11 +1,27 @@
 import replicate
-from chatarena.backends import IntelligenceBackend
+from chatarena.backends import IntelligenceBackend, OpenAIChat
 from chatarena.message import SYSTEM_NAME as SYSTEM, Message
 from typing import List, Union
+import openai
 import re
 import os
 
 DEFAULT_MAX_TOKENS = 4096
+END_OF_MESSAGE = "<EOS>"  # End of message token specified by us not OpenAI
+STOP = ("<|endoftext|>", END_OF_MESSAGE)  # End of sentence token
+BASE_PROMPT = f"The messages always end with the token {END_OF_MESSAGE}."
+DEFAULT_MODEL = "gpt-3.5-turbo"
+
+#Set up the openai client
+try:
+    client = openai.OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    is_openai_available = True
+except openai.OpenAIError:
+    # logging.warning("OpenAI API key is not set. Please set the environment variable OPENAI_API_KEY")
+    is_openai_available = False
+
+
+
 
 
 def is_replicate_available():
@@ -96,4 +112,28 @@ class ReplicateBackend(IntelligenceBackend):
         ).strip()  # noqa: F451
 
 
+        return response
+    
+
+
+
+class OpenAIBackend(OpenAIChat):
+
+    def __init__(self, model = DEFAULT_MODEL, **kwargs):
+        super().__init__(model=model, **kwargs)
+        self.tokens_used = 0
+
+    def _get_response(self, messages):
+        completion = client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            stop=STOP,
+        )
+
+        self.tokens_used += completion.usage.total_tokens
+
+        response = completion.choices[0].message.content
+        response = response.strip()
         return response
